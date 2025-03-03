4685ce67efa7ca98253c11d08c420157756c66af
dearfl <i@flr.me>
2025-03-03 22:28:50 +0800

    define host key in openssh

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,6 +7,12 @@ _: {
         PermitRootLogin = "no";
         PasswordAuthentication = false;
       };
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
     };
   };
 }
